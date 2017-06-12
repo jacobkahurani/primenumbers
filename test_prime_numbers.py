e62ae7c7e523f9e76797590223b684f6f9940f1b
jacobkahurani <jacobkahurani27@gmail.com>
2017-06-13 00:01:33 +0300

    Making test_prime_number.py production ready

--- test_prime_numbers.py
+++ test_prime_numbers.py
@@ -1,19 +1,19 @@
 """Code to test my_prime_numbers function"""
-import unittest
-from primenumbers import my_prime_numbers
-class test_my_prime_numbers(unittest.TestCase):
-    def test_for_correctness(self):
+import unittest#import unittest module
+from primenumbers import my_prime_numbers #import from primenumbers
+class test_my_prime_numbers(unittest.TestCase): #define class thet inherits from unittest.TestCase class
+    def test_for_correctness(self): #define function to test for correctness of value
         self.assertTrue(3 in my_prime_numbers(10))
         self.assertTrue(5 in my_prime_numbers(10))
         self.assertTrue(7 in my_prime_numbers(10))
-    def test_for_negative_numbers(self):
+    def test_for_negative_numbers(self):# define fuction to test for negative numbers
         self.assertRaises(ValueError,my_prime_numbers(-8),"only positive values")
-    def test_non_zero_or_1_input(self):
+    def test_non_zero_or_1_input(self): #Define function to test for zero and one input
         self.assertRaises(ValueError,my_prime_numbers(0),"only integers allowed")
         self.assertRaises(ValueError,my_prime_numbers(1),"only integers allowed")
-    def test_output_type_is_list(self):
+    def test_output_type_is_list(self):#Define function to test type of outputed data
         self.assertTrue(isinstance(my_prime_numbers(10)),list)
-    def test_2_is_prime(self):
+    def test_2_is_prime(self): #define test to check if 2 is a prime number
         self.assertTrue(2 in my_prime_numbers(10))
 if__name__ == '__main__':
     unittest.main()
